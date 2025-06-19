b094572ee9aa8ad7f96814b3f788aa3b5e5a48ff
Lakshay Verma <lakshayverma.v1@gmail.com>
2025-06-20 03:42:56 +0530

    Pointer fix

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -128,7 +128,7 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
         <Link
           onMouseEnter={() => setHovered(idx)}
           onClick={onItemClick}
-          className="relative px-4 py-2 text-neutral-600 dark:text-neutral-300"
+          className="relative px-4 py-2 text-neutral-600 dark:text-neutral-300 cursor-pointer"
           key={`link-${idx}`}
           to={item.link}
           smooth={true}
