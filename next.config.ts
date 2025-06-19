4ed8f99d60f650567195378f07044c102dc651bb
Lakshay Verma <lakshayverma.v1@gmail.com>
2025-06-20 03:28:34 +0530

    Update next-config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_VAPI_API_KEY: process.env.NEXT_PUBLIC_VAPI_API_KEY,
-  },
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
